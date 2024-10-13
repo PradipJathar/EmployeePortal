26f5af681b1bee77f088f4edcc269deb2df7b277
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-10-13 19:12:47 +0530

    Register employee repository.

--- EmployeePortalWeb/Program.cs
+++ EmployeePortalWeb/Program.cs
@@ -1,4 +1,5 @@
 using EmployeePortalWeb.Data;
+using EmployeePortalWeb.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<EmployeeRepository>();
 
 var app = builder.Build();
 
