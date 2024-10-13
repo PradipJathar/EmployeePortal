5a1e76db0eb2784433daaeeada766918792bd40d
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-10-13 18:49:49 +0530

    Add Employee Repository.

---
+++ EmployeePortalWeb/Repository/EmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using EmployeePortalWeb.Data;
+using EmployeePortalWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortalWeb.Repository
+{
+    public class EmployeeRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public EmployeeRepository(ApplicationDbContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await db.Employees.ToListAsync();
+        }
+
+        public async Task SaveEmployee(Employee employee)
+        {
+            await db.Employees.AddAsync(employee);
+            await db.SaveChangesAsync();
+        }
+    }
+}
