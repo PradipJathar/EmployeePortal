87db6c0de21f0d6acff49bb716514c16aceabe36
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-10-13 18:58:58 +0530

    Add Employee controller.

---
+++ EmployeePortalWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using EmployeePortalWeb.Models;
+using EmployeePortalWeb.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortalWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository repo;
+
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            this.repo = employeeRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allEmployees = await repo.GetEmployeesAsync();
+            return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SaveEmployee(Employee employee)
+        {
+            await repo.SaveEmployee(employee);
+            return Ok(employee);
+        }
+    }
+}
